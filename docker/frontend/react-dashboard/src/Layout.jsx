063e24ae282e217a4f6ab5f97d760babb19eb22c
Kevin Puga <143359293+KxVxN-coder@users.noreply.github.com>
2025-03-04 14:10:53 -0600

    updated dashboard

--- docker/frontend/react-dashboard/src/Layout.jsx
+++ docker/frontend/react-dashboard/src/Layout.jsx
@@ -1,25 +1,35 @@
 import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand href="#">Dashboard</Navbar.Brand>
-            </Navbar>
-            <Container fluid>
-                <Row>
-                    <Col md={2} className="bg-dark min-vh-100 p-3">
-                        <Nav className="flex-column">
-                            <Nav.Link href="#">Home</Nav.Link>
-                        </Nav>
-                    </Col>
-                    <Col md={10} className="p-3">
-                        {children}
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      {/* Top Navbar */}
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">Dashboard</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      {/* Sidebar and Main Content */}
+      <Container fluid>
+        <Row className="vh-100"> {/* Full height for the row */}
+          {/* Sidebar (Fixed Height & Scrollable) */}
+          <Col md={2} className="bg-dark text-white p-3 d-flex flex-column" style={{ height: '150vh' }}>
+            <Nav className="flex-column">
+              <Nav.Link as={Link} to="/" className="text-white">Home</Nav.Link>
+              <Nav.Link as={Link} to="/analytics" className="text-white">Real-Time Analytics</Nav.Link>
+            </Nav>
+          </Col>
+
+          {/* Main Content Area */}
+          <Col md={10} className="p-4" style={{ minHeight: '100vh' }}>
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Layout;
