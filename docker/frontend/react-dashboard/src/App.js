af1df49b76c7e0b66f743ae16c61bdd85433c2e4
‮daot <47069617+daot@users.noreply.github.com>
2025-03-05 15:25:09 -0600

    Update App.js

--- docker/frontend/react-dashboard/src/App.js
+++ docker/frontend/react-dashboard/src/App.js
@@ -2,8 +2,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Dashboard from "./Dashboard";
-import Timeline from "./api/fetch-real-time.jsx"
-import BlueskyFeed from "./api/makeContinuousFeed.jsx";
 import RealTimeAnalytics from "./RealTimeAnalytics";
 
 function App() {
