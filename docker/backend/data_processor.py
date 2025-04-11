3d4f42a1fe2c11b7e1128f389588a1f4b0f8373f
nushbaj <ahsuna.bajracharya@gmail.com>
2025-04-11 17:44:07 -0500

    updated data processor to have a since and latest filter. routes takes an optional start date and end date parameter

--- docker/backend/data_processor.py
+++ docker/backend/data_processor.py
@@ -1,61 +1,152 @@
 import pandas as pd
 import requests
-import sqlite3
 from collections import Counter
 import json
 from datetime import datetime, timedelta
+from geocode_db import get_location_coordinates
 from dotenv import load_dotenv
 import os
 import nltk
 from nltk.corpus import stopwords
 import inflect
+import redis
+import asyncio
 
-#load_dotenv()
-#API_URL=os.getenv('API_URL')
-p = inflect.engine()
-DATABASE = "location_cache.db"
+load_dotenv()
 nltk.download("stopwords")
 
-stop_words = set(stopwords.words("english"))
-additional_stop_words = ['tornado', 'hurricane', 'wildfire', 'blizzard', 'earthquake', 'flood', 'watchedskysocialappalerts']
-stop_words.update((additional_stop_words))
-stop_words.update({p.number_to_words(i) for i in range(0,1001)})
-
-class DataProcessor:
+#Each fetch function validates the start and end parameters as sent from the user 
+class DataProcessor1:
     def __init__(self):
-
         """Fetches data once to be used across all API calls"""
-        self.df = self.fetch_data()
-    def fetch_data(self):
+        redis_host = os.getenv('REDIS_HOST', 'localhost')
+        redis_port = int(os.getenv('REDIS_PORT', 6379))
+        #self.MCR=10
+        self.redis_cli = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+        self.cache_df = self.load_cache_data()
 
-        """Fetching data from the API URL and converting to dataframe"""
-        response = requests.get("http://db:5001/get_latest_posts")
-    
-        if response.status_code != 200:
-            print(f"error: failed to fetch data with status code {response.status_code}")
+        #all timestamps should be in pd.Timestamp format 
+        self.latest_timestamp = self.load_latest_timestamp()
+
+        self.api_url = os.getenv('API_URL')
+        self.location_database = os.getenv('CACHE_FILE')
+
+        p = inflect.engine()
+        self.stop_words = set(stopwords.words("english"))
+        additional_stop_words = [
+            'tornado', 
+            'hurricane', 
+            'wildfire', 
+            'earthquake', 
+            'flood',
+            'flooding',
+            'watchedskysocialappalerts', 
+            'hundred',
+            'thousand'
+        ]
+        self.stop_words.update((additional_stop_words))
+        self.stop_words.update({p.number_to_words(i) for i in range(0,1001)})
+        self.fetch_data()
+
+    def load_latest_timestamp(self):
+        latest_timestamp = self.redis_cli.get("latest_timestamp")
+        if latest_timestamp:
+            latest_ts = pd.to_datetime(latest_timestamp, utc=True)
+            return latest_ts
+        else:
             return None
+    
+    def load_cache_data(self):
+        cache_data = self.redis_cli.get("cache_data")
+        if cache_data:
+            try:
+                df = pd.read_json(cache_data)
+                return df
+            except Exception as e:
+                return pd.DataFrame()
+        else:
+            return pd.DataFrame()
+        
+    def fetch_data(self):
+        """Fetching data from the API URL and converting to dataframe"""
+        if self.latest_timestamp:
+            timestamp = self.latest_timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+            response = requests.get(f"{self.api_url}/get_latest_posts?start_timestamp={timestamp}")
+
+            if response.status_code != 200:
+                return None
+        else:
+            response = requests.get(f"{self.api_url}/get_latest_posts") # grab everything if no latest
+        
+        # Update the latest timestamp
         data = response.json()
-        posts = data.get("posts", []) 
-        return pd.DataFrame(posts)
+        new_latest_timestamp = pd.to_datetime(data.get("latest_timestamp"), utc=True)
 
-    def filter_data(self, since=None, label=None, location=False, specific_location=None):
+        if self.latest_timestamp is None or new_latest_timestamp > self.latest_timestamp:
+            self.latest_timestamp = new_latest_timestamp + timedelta(seconds=1) # so we don't grab last post
+            
+        posts = data.get("posts", [])
+        if not posts:
+            return self.cache_df
+        
+        new_df = pd.DataFrame(posts)
+        new_df["timestamp"] = pd.to_datetime(new_df["timestamp"], utc=True)
 
+        #Merge with existing df
+        self.cache_df = pd.concat([self.cache_df, new_df], ignore_index=True)
+        self.redis_cli.set("cache_data", self.cache_df.to_json(orient="records"))
+        self.redis_cli.set("latest_timestamp", self.latest_timestamp.strftime("%a, %d %b %Y %H:%M:%S GMT"))
+
+        return self.cache_df
+
+    def filter_data(self, since=None, latest=None, label=None, location=False, specific_location=None, sentiment=False):
         """Data filtering based on what the other functions need"""
-        df = self.df.copy()
+        df = self.cache_df.copy() # changed to cache_df
         df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True)
         if since:
             df = df[df["timestamp"] >= pd.to_datetime(since, utc=True)]
+        if latest:
+            df = df[df["timestamp"] <= pd.to_datetime(latest, utc=True)]
         if label:
             df = df[df["label"] == label]
         if location:
             df = df[df["location"].notna()]
         if specific_location:
             df = df[df["location"]==specific_location]
+        if sentiment:
+            df['sentiment'] = pd.to_numeric(df['sentiment'], errors='coerce')
+            df['sentiment_scaled'] = (df['sentiment'] - df['sentiment'].min()) / (df['sentiment'].max() - df['sentiment'].min())
         return df
+
+    def validate_date_range(self, start_date, end_date):
+        """Validating the date range as provided by user"""
+
+        #No date range provided, default to last 24 hours
+        if start_date is None or end_date is None:
+            start_date = pd.to_datetime(datetime.utcnow() - timedelta(days=1))
+            end_date = pd.to_datetime(datetime.utcnow())
+            return start_date, end_date
+        
+        #Inputed date range accepted for pd.to_datetime format
+        try:
+            start_date = pd.to_datetime(start_date, utc=True)
+            end_date = pd.to_datetime(end_date, utc=True)
+        except ValueError:
+            return {"error": "Invalid date format. Enter date in YYYY-MM-DD format."} 
+
+        if start_date > end_date:
+            return {"error": "Start date must be before end date."}
+
+        return start_date, end_date
     
-    def fetch_label_count(self):
+    def fetch_label_count(self, start_date=None, end_date=None):
         """Fetching the count and percentage for each label"""
-        df = self.df
+        df = self.cache_df # changed to cache_df
+        validation = self.validate_date_range(start_date, end_date)
+        if isinstance(validation, dict) and "error" in validation:
+            return validation
+        start_date, end_date = validation
+        df = self.filter_data(since=start_date, latest=end_date)
         count = df["label"].value_counts().to_dict() 
         total_count = df["label"].count()
         results = [
@@ -66,31 +157,41 @@ class DataProcessor:
             "total label count": int(total_count),
             "results": results
         }
-    def fetch_most_frequent(self, disaster_type):
-
+    def fetch_most_frequent(self, disaster_type, start_date=None, end_date=None):
         """Finds the most common words based on label or over entire dataset"""
-        df = self.filter_data(label=disaster_type)
+        validation = self.validate_date_range(start_date, end_date)
+        if isinstance(validation, dict) and "error" in validation:
+            return validation
+        start_date, end_date = validation
+
+        df = self.filter_data(since=start_date, latest=end_date, label=disaster_type)
         all_cleaned_text = " ".join(df["cleaned"].astype(str))
-        words = [word for word in all_cleaned_text.split() if word not in stop_words and not word.isdigit() and len(word)> 3]
+        words = [word for word in all_cleaned_text.split() if word not in self.stop_words and not word.isdigit() and len(word)> 3]
         count = Counter(words)
         return [{"keyword": str(word), "count": int(freq)} for word, freq in count.most_common(20)]
     
-    def fetch_posts_over_time(self, disaster_type):
-
+    def fetch_posts_over_time(self, disaster_type, start_date=None, end_date=None):
         """Finds the posts per day based on disaster type"""
-        df = self.filter_data(label=disaster_type)
-        if df.empty:
-            return {"error": "No posts found for the given disaster type"}
-        posts_per_day = df.resample("D", on = "timestamp").size().reset_index(name="post_count")
+        validation = self.validate_date_range(start_date, end_date)
+        if isinstance(validation, dict) and "error" in validation:
+            return validation
+        
+        start_date, end_date = validation
+        df = self.filter_data(since=start_date, latest=end_date, label=disaster_type)
+        posts_per_day = df.resample("D", on="timestamp").size().reset_index(name="post_count")
         posts_per_day["timestamp"] = posts_per_day["timestamp"].dt.strftime("%Y-%m-%d")
         
         return posts_per_day.to_dict(orient="records")
 
-    def fetch_top_disaster_last_day(self):
-
+    def fetch_top_disaster_location(self, start_date=None, end_date=None):
         """Gets the most popular disaster type, location, and danger level within the past 24 hours"""
-        last_day = datetime.utcnow() - timedelta(days=1)
-        df = self.filter_data(since=last_day, location=True) #filtering by non-null locations
+        validation = self.validate_date_range(start_date, end_date)
+        if isinstance(validation, dict) and "error" in validation:
+            return validation
+        start_date, end_date = validation
+
+        #filtering by non-null locations
+        df = self.filter_data(since=start_date, latest=end_date, location = True) 
         if df.empty:
             return {"Error": "No valid disasters mentioned in the last day"}
         
@@ -101,16 +202,10 @@ class DataProcessor:
         top_label, top_location = top_pair[0][0]
 
         #Filter data again to get only the entries related to the pair
-        df_labloc = self.filter_data(since=last_day, label=top_label, specific_location=top_location)
-        df_labloc['sentiment'] = pd.to_numeric(df['sentiment'], errors='coerce')
-
-        #Using minmax scaling to emphasize the sentiment scores more
-        min_sent = df_labloc['sentiment'].min()
-        max_sent = df_labloc['sentiment'].max()
-        df_labloc['sentiment_scaled'] = (df_labloc['sentiment']-min_sent)/(max_sent-min_sent)
+        df_filtered = self.filter_data(since=start_date, latest=end_date, label=top_label, specific_location=top_location, sentiment=True)
 
         #Danger Level calculated by mean of sentiment scores
-        avg_sentiment = df_labloc['sentiment_scaled'].mean()
+        avg_sentiment = df_filtered['sentiment_scaled'].mean()
         if avg_sentiment <= -0.5:
             danger_level = "high"
         elif avg_sentiment < 0.5:
@@ -125,22 +220,27 @@ class DataProcessor:
             "danger_value": float(avg_sentiment)
             }
 
-    def fetch_text_last_day(self, disaster_type):
+    def fetch_text(self, disaster_type, start_date=None, end_date=None):
 
-        last_hour = datetime.utcnow() - timedelta(days=1)
-        df = self.filter_data(since=last_hour, label=disaster_type)
+        validation = self.validate_date_range(start_date, end_date)
+        if isinstance(validation, dict) and "error" in validation:
+            return validation
+        
+        start_date, end_date = validation
+        df = self.filter_data(since=start_date, latest=end_date, label=disaster_type)
         if df.empty:
             return {"error": "disaster type not found"}
+        
         df['timestamp'] = df['timestamp'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if pd.notnull(x) else '')
-        return df[['text', 'author', 'timestamp']].to_dict(orient="records")
+        return df[['text', 'handle', 'timestamp']].to_dict(orient="records")
     
-    def fetch_location_coordinates(self):
-         
+     def fetch_location_coordinates(self):
         """Fetches coordinates from geocoded_cache.db"""
-        conn = sqlite3.connect(DATABASE)
+        conn = sqlite3.connect(self.location_database)
         cursor = conn.cursor()
         cursor.execute("SELECT latitude, longitude FROM locations")
         rows = cursor.fetchall()
         conn.close()
 
         return [{"latitude": row[0], "longitude": row[1]} for row in rows]
+
--- docker/backend/routes.py
+++ docker/backend/routes.py
@@ -1,37 +1,77 @@
 from flask import Flask, jsonify, request, Response
-from data_processor import DataProcessor
+from flask_apscheduler import APScheduler
+from data_processor import DataProcessor1
 import json
+import asyncio
 from flask_cors import CORS
+import logging
+import sys
 
 app = Flask(__name__)
 CORS(app)
-process = DataProcessor()
+
+logger = logging.getLogger(__name__)
+process = DataProcessor1()
+scheduler = APScheduler()
+scheduler.api_enabled = True
+scheduler.init_app(app)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+@scheduler.task('interval', id='get_new_posts', seconds=60, misfire_grace_time=900)
+def update_cache():
+    logger.info("Update cache process started")
+    process.fetch_data()
+    print('Fetched data')
+
+scheduler.start()
+
 
 @app.route("/fetch-label-count", methods=["GET"])
 def label_count():
-    return jsonify(process.fetch_label_count())
+    start_date = request.args.get("start_date", default=None)
+    end_date = request.args.get("end_date", default=None)
+    return jsonify(process.fetch_label_count(start_date, end_date))
 
 @app.route("/fetch-most-frequent-word", methods=["GET"])
 def most_frequent():
+    start_date = request.args.get("start_date", default=None)
+    end_date = request.args.get("end_date", default=None)
     disaster_type = request.args.get("disaster_type")
-    return jsonify(process.fetch_most_frequent(disaster_type))
+    if not disaster_type:
+        return jsonify({"error": "disaster_type parameter must be provided"}), 400
+    return jsonify(process.fetch_most_frequent(disaster_type, start_date, end_date))
 
 @app.route("/fetch-posts-over-time")
 def posts_time():
+    start_date = request.args.get("start_date", default=None)
+    end_date = request.args.get("end_date", default=None)
     disaster_type = request.args.get("disaster_type")
     if not disaster_type:
-        return jsonify({"error": "label must be provided"}), 400
-    return jsonify(process.fetch_posts_over_time(disaster_type))
+        return jsonify({"error": "disaster_type parameter must be provided"}), 400
+    return jsonify(process.fetch_posts_over_time(disaster_type, start_date, end_date))
 
-@app.route("/fetch-top-disaster-last-day", methods = ["GET"])
+@app.route("/fetch-top-disaster-location", methods = ["GET"])
 def top_disaster():
-    return jsonify(process.fetch_top_disaster_last_day())
+    start_date = request.args.get("start_date", default=None)
+    end_date = request.args.get("end_date", default=None)
+    return jsonify(process.fetch_top_disaster_location(start_date, end_date))
 
 @app.route("/fetch-text-from-label", methods=["GET"])
 def view_feed_data():
+    start_date = request.args.get("start_date", default=None)
+    end_date = request.args.get("end_date", default=None)
     disaster_type = request.args.get("disaster_type")
+    if disaster_type is None:
+        return jsonify({"error": "disaster_type parameter is required"}), 400
     return Response(
-        json.dumps(process.fetch_text_last_day(disaster_type), ensure_ascii=False, indent=4),
+        json.dumps(process.fetch_text(disaster_type, start_date, end_date), ensure_ascii=False, indent=4),
         mimetype='application/json'
     )
     
@@ -43,6 +83,6 @@ def fetch_coordinates():
         return jsonify(coordinates)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
